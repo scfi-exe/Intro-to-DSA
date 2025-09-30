a439c7fc978fa2b1790dbd4957e961eb43ff0887
scfi-odoo <scfi@odoo.com>
2025-09-30 17:40:27 -0400

    sorted sets are op

--- Static Arrays/Ex. Remove Duplicates From Sorted Array.py
+++ Static Arrays/Ex. Remove Duplicates From Sorted Array.py
@@ -1,13 +1,7 @@
 def removeDuplicates(nums: list) -> int:
-    for i in nums:
-        val = nums[i - 1]
-        if nums.count(val) > 1:
-            length = len(nums)
-            for index in range(i + 1, length):
-                nums[index - 1] = nums[index]
-            nums.pop()
-    return len(nums)
+    unique = sorted(set(nums))
+    nums[: len(unique)] = unique
+    return unique
 
 
-print(removeDuplicates([1, 1, 1, 2, 3]))
-print(removeDuplicates([2, 10, 10, 30, 30, 30]))
+print(removeDuplicates([1, 1, 1, 3, 2, 3, 3, 2, 3, 2]))
--- Static Arrays/wtf.py
+++
@@ -1,6 +0,0 @@
-def workIt(nums: list) -> int:
-    for i in nums:
-        print(nums[i - 1])
-
-
-workIt([1, 2, 3])
